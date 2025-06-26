6abc7e6a84efc33d7de83e55e0496403b5981afe
Fernando <fernando8715@hotmail.com>
2025-06-25 20:34:46 -0500

    configuración del idioma del calendario y organizacion de las importaciones en la carpeta helpers

--- src/calendar/page/CalendarPage.jsx
+++ src/calendar/page/CalendarPage.jsx
@@ -1,23 +1,12 @@
-import { Calendar, dateFnsLocalizer } from 'react-big-calendar';
+import { Calendar, } from 'react-big-calendar';
 import 'react-big-calendar/lib/css/react-big-calendar.css';
 
-import { addHours, format, parse, startOfWeek, getDay } from 'date-fns';
-import enUS from 'date-fns/locale/en-US'
+import { addHours } from 'date-fns';
 
 
 import { Navbar } from "../"
 
-const locales = {
-  'en-US': enUS,
-}
-
-const localizer = dateFnsLocalizer({
-  format,
-  parse,
-  startOfWeek,
-  getDay,
-  locales,
-})
+import { getMessagesES, localizer } from '../../helpers';
 
 const events = [{
   title: 'cumpleaños esposa',
@@ -37,11 +26,13 @@ export const CalendarPage = () => {
       <Navbar />
 
       <Calendar
+        culture='es'
         localizer={localizer}
         events={events}
         startAccessor="start"
         endAccessor="end"
         style={{ height: 'calc(100vh - 80px)' }}
+        messages={ getMessagesES() }
       />
 
     </>
